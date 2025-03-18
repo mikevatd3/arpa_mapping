d197160838863926c58203d5a9db1c65656e8ab7
mikevatd3 <mikevatd3@github.com>
2025-03-18 14:01:04 -0400

    documenting issue with filtering to city

--- acp_households_by_zip.sql
+++ acp_households_by_zip.sql
@@ -1,6 +1,11 @@
+-- P:\2024_Projects\ARPA\Development\Workspace\Website Workspace - Esri Hub\Esri Hub Report Datasets\acp_households_by_zip_<date>.geojson
+
 -- Not all zipcodes have the same date for most recent data
 -- this finds the individual 'max_dates' and then filters them to
 -- the end
+
+-- There is something wrong with 'city_id' on this file so this is state-wide
+
 WITH maxes AS (
     SELECT zipcode, max(data_month) max_date
     FROM acp_households_by_zipcode
@@ -15,4 +20,5 @@ RIGHT JOIN maxes mx
     ON mx.zipcode = hhs.zipcode
     AND mx.max_date = hhs.data_month
 LEFT JOIN dim_zip
-  ON hhs.zipcode = dim_zip.zipcode;
+  ON hhs.zipcode = dim_zip.zipcode
+-- WHERE city_id = 128; -- Doesn't work as expected
